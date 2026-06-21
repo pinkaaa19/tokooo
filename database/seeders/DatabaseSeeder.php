b012da604437e69580886dae059f4c35109afabb
pinkaaa19 <jofinkap@gmail.com>
2026-06-21 15:06:13 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,30 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 1. Pembeda Admin: Nilai role diisi 'admin'
+        User::create([
+            'name' => 'Admin Toko Toraja',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password123'),
+            'role' => 'admin', // <-- PEMBEDA UTAMA (Sebagai Admin)
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 2. Pembeda User: Nilai role diisi 'user' atau 'customer'
+        User::create([
+            'name' => 'Kustomer Toko Toraja',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password123'),
+            'role' => 'user', // <-- PEMBEDA UTAMA (Sebagai Customer)
         ]);
     }
 }
