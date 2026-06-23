616d8c4d86b600d8f19e9d7c92979e57dd0c5d5b
pinkaaa19 <jofinkap@gmail.com>
2026-06-23 16:21:30 +0800

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // Relasi ke SOP
+    public function sops()
+    {
+        return $this->hasMany(SopContent::class, 'category_id'); // sesuaikan foreign_key jika berbeda
+    }
+
+    // Relasi ke FAQ
+    public function faqs()
+    {
+        return $this->hasMany(FaqContent::class, 'category_id'); // sesuaikan foreign_key jika berbeda
+    }
 }
